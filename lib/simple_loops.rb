2f6851521f4656ec24ee4450e7e1bc8f02660ad0
Yehong Piao <piaoyehong1107@gmail.com>
2020-06-25 04:40:07 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,10 +14,14 @@ count = 0
 end
 end
 
-def output_array
-  
+def output_array(array)
+  count = 0 
+  while (count < array.length) do
+    puts array[count]
+    count +=1 
+  end
 end
 
-def return_string_array
+def return_string_array(array)
   
 end
