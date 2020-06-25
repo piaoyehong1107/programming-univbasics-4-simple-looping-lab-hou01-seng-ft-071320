144b1fe25d663c984f6dac42a9bc7c10c26a7b5c
Yehong Piao <piaoyehong1107@gmail.com>
2020-06-25 04:41:06 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -23,5 +23,10 @@ def output_array(array)
 end
 
 def return_string_array(array)
-  
+  count = 0
+  while (count < array.length) do
+    newArray[count]=array[count].to_s
+    count +=1 
+  end
+  newArray
 end
