6b7506103f829bd8127154a314eba4c9014a5326
Yehong Piao <piaoyehong1107@gmail.com>
2020-06-25 04:34:08 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,19 @@
-# Write your methods here
+count = 0
+def loop_message_five_times(message)
+  while count < 5
+  do puts message
+  count +=1 
+end
+end
+
+def loop_message_n_times
+  
+end
+
+def output_array
+  
+end
+
+def return_string_array
+  
+end
